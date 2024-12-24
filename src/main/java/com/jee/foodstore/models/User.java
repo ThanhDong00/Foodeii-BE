569dab41561e25323458f43cb34697a960c36e60
ThanhDong00 <thanhdonganmy@gmail.com>
2024-12-24 19:31:20 +0700

    Update user model

--- src/main/java/com/jee/foodstore/models/User.java
+++ src/main/java/com/jee/foodstore/models/User.java
@@ -1,12 +1,14 @@
 package com.jee.foodstore.models;
 
 import dev.morphia.annotations.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @Entity("users")
 @EqualsAndHashCode(callSuper = true)
+@Builder(toBuilder = true)
 @Indexes({
         @Index(fields = @Field("email"), options = @IndexOptions(unique = true))
 })
@@ -16,6 +18,7 @@ public class User extends BaseEntity{
     private String passwordHash;
     private String address;
     private String phone;
+
+    @Builder.Default
+    private String role = "USER";
 }
-
-
